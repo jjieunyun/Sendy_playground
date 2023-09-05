def4063c81ffa2fff89d522b27426e260c5dd0e6
Jane <lesson0186@gmail.com>
2023-09-05 19:08:29 +0900

    feat:SlideBar구현완료

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,9 +1,8 @@
 "use client";
-
 import hamburger from '@image/hamburger 1.svg'
 import logo from '@image/sendyplayground-logo.svg'
 import Image from "next/image";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 import SideBar from "@components/common/SideBar";
 import {useRouter} from "next/navigation";
 
@@ -19,9 +18,28 @@ export default function Header() {
         setSidebarVisible(false);
     };
     
+    useEffect(() => {
+        const handleEscape = (event:any) => {
+            if (event.keyCode === 27) {  // 27 is the keyCode for the Escape key
+                closeSidebar();
+            }
+        };
+        window.addEventListener('keydown', handleEscape);
+        
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('keydown', handleEscape);
+        };
+    }, []);
+    
     return (
-        <div className="w-full h-full">
-            <nav className="w-full flex justify-between px-24 pt-8">
+        <div className="w-full h-full relative">
+            <div
+                className={`fixed inset-0 bg-black transition-opacity duration-500 ease-in-out ${sidebarVisible ? 'opacity-50' : 'opacity-0 pointer-events-none'}`}
+                onClick={closeSidebar}
+            ></div>
+            
+            <nav className="w-full flex justify-between px-24 pt-8 z-20">
                 <Image src={logo} alt="logo" onClick={()=>router.push('/main')} className="cursor-pointer"/>
                 <Image
                     src={hamburger}
@@ -30,9 +48,10 @@ export default function Header() {
                     onClick={toggleSidebar}
                 />
             </nav>
-            
-            <div className={`fixed top-0 right-0 h-full bg-gray-900 transition-all duration-500 ease-in-out ${sidebarVisible ? 'w-440' : 'w-0'} overflow-hidden`}>
-                <SideBar closeSidebar={closeSidebar}/>
+            <div className={`fixed top-0 right-0 h-full bg-gray-900 transition-all duration-500 ease-in-out ${sidebarVisible ? 'w-440' : 'w-0'} overflow-hidden z-10`}>
+                <div className="w-440 h-full">
+                    <SideBar closeSidebar={closeSidebar}/>
+                </div>
             </div>
         </div>
     );
